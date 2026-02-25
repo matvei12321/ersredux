377df272d4a82241d2ddbf5c5f8d5797043dc545
Matvei <53666427+matvei12321@users.noreply.github.com>
2026-02-25 13:20:52 +0300

    ers redux

---
+++ ersredux/common/defines/ers_defines.lua
@@ -0,0 +1,11 @@
+
+NDefines.NDiplomacy.TENSION_STATE_VALUE = 1.2									-- Vanilla is 2
+NDefines.NDiplomacy.TENSION_NO_CB_WAR = 4								-- Vanilla is 15
+NDefines.NDiplomacy.TENSION_CB_WAR = 4									-- Vanilla is 5
+NDefines.NDiplomacy.TENSION_ANNEX_NO_CLAIM = 0.5								-- Vanilla is 2
+NDefines.NDiplomacy.TENSION_ANNEX_CLAIM = 0.5								-- Vanilla is 1
+NDefines.NDiplomacy.TENSION_ANNEX_CORE = 0.3								-- Vanilla is 0.5
+NDefines.NDiplomacy.TENSION_PUPPET = 1.25									-- Vanilla is 1.25
+NDefines.NDiplomacy.TENSION_FORCE_GOVERNMENT = 0							-- Vanilla is 0.75
+NDefines.NDiplomacy.TENSION_GUARANTEE = -5									-- Vanilla is -5
+NDefines.NGame.EVENT_TIMEOUT_DEFAULT = 20                                                  -- Vanilla is 13
---
+++ ersredux/descriptor.mod
@@ -0,0 +1,23 @@
+version="1.0.0"
+tags={
+	"Alternative History"
+	"Graphics"
+	"Ideologies"
+	"Map"
+	"Gameplay"
+	"National Focuses"
+	"Technologies"
+	"Sound"
+	"Events"
+	"Military"
+}
+dependencies={
+	"Equestria at War"
+}
+replace_path="history/states"
+replace_path="common/bookmarks"
+replace_path="gfx/loadingscreens"
+name="Equestria Rises Still Redux"
+picture="thumbnail.png"
+supported_version="1.17.*"
+remote_file_id="3539455837"
